535d150c753d3288640b7dd8eea03d2368ff634f
lewinskie254 <machiulewis@gmail.com>
2026-06-23 12:22:19 +0300

    still making headway

--- intro.py
+++ intro.py
@@ -28,12 +28,12 @@ sentence = 'Start a sentence and then bring it to an end'
 #predominantly we are going to be using raw strings 
 
 # pattern = re.compile(r'[a-z A-Z 0-9]+\.com')
-pattern = re.compile(r'\s')
+pattern = re.compile(r'[\w]+\.\s')
 
 matches = pattern.finditer(text_to_search)
 
 
-# \s - Whitespace, \S - Not whitespace, space, tab 
+# \s - Whitespace (space, tab, \n), \S - Not whitespace, space, tab, \n 
 # \d - (0 - 9), \D not a digit 
 # \w Word character, \W not  word character (\n, )
 for match in matches:
